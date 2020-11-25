e641f633aba1120680a2c878d43644a358011692
pravin-yadav <work.coderpravin@gmail.com>
2020-11-25 23:46:36 +0530

    Added api to fetch Quote of the day.

--- src/QuotesGen.js
+++ src/QuotesGen.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
 class QuotesGen extends React.Component{
+    state= {
+        quotes: [],
+        hasQuote: false
+    }
+    fetchQuotes = (e) => {
+        e.preventDefault();
+        axios.get('https://quotes.rest/qod?language=en').then(response => {
+            this.setState({quotes: response.data.contents.quotes, hasQuote: true})
+        }).catch(error => {
+            console.log(error)
+        })
+    }
     render(){
+        const { quotes,hasQuote } = this.state || [];
+        let quote = quotes.map(quote => quote.quote);
         return(
             <React.Fragment>
-                <div class="quote">
-                    <h1>Quotes Generator</h1>
+                <div className="quote">
+                    {hasQuote ? quote : "Click to generate quote.. ."}
                 </div>
 
-                <button type="submit" onClick="refreshPage()">
+                <button type="button" onClick={this.fetchQuotes}>
                     Quote Of The Day
                 </button>
             </React.Fragment>
