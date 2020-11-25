1400c1e2d55382fcb02aaea3ac2c706d1aa777a9
pravin-yadav <work.coderpravin@gmail.com>
2020-11-25 23:16:33 +0530

    Added Quote Generator Component.

---
+++ src/QuotesGen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class QuotesGen extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <h1>Quote Generator!!</h1>
+            </React.Fragment>
+        )
+    }
+}
+
+export default QuotesGen;
