fb9a481b000e4d66f9ae8d7cfb0b74c1e29dbd4e
pravin-yadav <work.coderpravin@gmail.com>
2020-12-01 20:29:18 +0530

    Added Tabs to display quotes by categories.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import QuotesGen from './QuotesGen';
+import Tabs from 'react-bootstrap/Tabs'
+import Tab from 'react-bootstrap/Tab'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  const [key, setKey] = useState('qod');
   return (
     <div className="App">
-      <QuotesGen />
+       <Tabs
+          id="quotes-gen"
+          activeKey={key}
+          onSelect={(k) => setKey(k)}
+        >
+        <Tab eventKey="qod" title="QOD">
+          <QuotesGen />
+        </Tab>
+        <Tab eventKey="random" title="RANDOM">
+          <QuotesGen />
+        </Tab>
+        <Tab eventKey="success" title="Success">
+          <QuotesGen />
+        </Tab>
+      </Tabs>
     </div>
   );
 }
